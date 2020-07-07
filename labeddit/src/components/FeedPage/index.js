ffb4a8bace4c3b4cebdddcbb84da7448cf7b4f53
Renata Mitsue <rmkarato@gmail.com>
2020-07-06 22:16:15 -0300

    api get posts e create post adicionadas

--- labeddit/src/components/FeedPage/index.js
+++ labeddit/src/components/FeedPage/index.js
@@ -1,10 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-//import { useProtectedPage } from "./../../useProtectedPage";
+import axios from "axios";
+import { useProtectedPage } from "../../hooks/useProtectedPage";
+import useForm from "../../hooks/useForm"
+
+const baseUrl = 
+  "https://us-central1-labenu-apis.cloudfunctions.net/labEddit";
 
 function FeedPage() {
-  //useProtectedPage();
+  useProtectedPage();
   let history = useHistory();
+
+  const token = localStorage.getItem("token")
+
+  const { form, onChange } = useForm ({ text: "", tittle: ""})
+  const [posts, setPosts] = useState([]);
+  
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const mySubmitCommentHandler = async(event) => {
+    event.preventDefault();
+
+    const axiosConfig = {
+      headers: {
+        Authorization: token,
+      }
+    };
+
+    const body = {
+      text: form.text,
+      title: form.title,
+    }
+
+    try {
+      const response = await axios.post(`${baseUrl}/posts`, form, body, axiosConfig);
+      console.log(response.data.success)
+      alert("Post criado com sucesso!")
+    } catch (error) {
+      console.log(error)
+      alert("Não foi possível criar o post.")
+    }
+  }
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    onChange(name, value)
+  }
+
+  const getPosts = async() => {
+    const axiosConfig = {
+      headers: {
+        Authorization: token,
+      }
+    };
+
+    try {
+      const response = await axios.get(`${baseUrl}/posts`, axiosConfig);
+      setPosts(response.data.posts)
+      console.log(response.data.posts)
+    } catch(error) {
+      console.log(error)
+    }
+  }
 
   const goToPost = () => {
     history.push("/post");
@@ -13,20 +74,35 @@ function FeedPage() {
   return (
     <div>
       <h3>Feed de posts</h3>
-      <form>
-        <label htmlFor="writePost">Escreva seu post</label>
-        <input id="writePost" type="text" required />
-        <button>Postar</button>
+      <form onSubmit={mySubmitCommentHandler}>
+        <label htmlFor="text">Escreva seu post</label>
+        <input 
+          id="text"
+          type="text"
+          name="text"
+          value={form.text}
+          minLength="3"
+          required
+          onChange={handleInputChange}
+        />
+        <button type="submit">Postar</button>
       </form>
 
       <hr />
 
       <div>
-        <p>Nome do usuário</p>
-        <p>Texto do post</p>
-        <p>0 comentários</p>
+          {posts.length === 0  && <div>Carregando...</div>}
+          {posts && posts.map(post => {
+            return (
+              <div>
+                <p>{post.username}</p>
+                <p>{post.text}</p>
+                <p>{post.commentsCount} comentários</p>
+                <button onClick={goToPost}>Detalhes post</button>
+              </div>
+            )
+          })}
       </div>
-      <button onClick={goToPost}>Detalhes post</button>
     </div>
   );
 }
--- labeddit/src/components/Router.js
+++ labeddit/src/components/Router.js
@@ -1,25 +1,36 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
+import DocumentTitle from "react-document-title";
+
 import LoginPage from "./LoginPage/index";
 import SignUpPage from "./SignUpPage/index";
 import FeedPage from "./FeedPage/index";
 import PostPage from "./PostPage/index";
+
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
-          <LoginPage />
+          <DocumentTitle title="LabEddit - Login">
+            <LoginPage />
+          </DocumentTitle>
         </Route>
         <Route exact path="/signup">
-          <SignUpPage />
+          <DocumentTitle title="LabEddit - Cadastro">
+            <SignUpPage />
+          </DocumentTitle>
         </Route>
         <Route exact path="/feed">
-          <FeedPage />
+          <DocumentTitle title="LabEddit - Feed">
+            <FeedPage />
+          </DocumentTitle>
         </Route>
         <Route exact path="/post">
-          <PostPage />
+          <DocumentTitle title="LabEddit - Post">
+            <PostPage />
+          </DocumentTitle>
         </Route>
       </Switch>
     </BrowserRouter>
